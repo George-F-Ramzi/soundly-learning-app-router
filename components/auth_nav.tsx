9e047e1add77e3e50d0ef220cbeced57618b4151
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-06-14 14:30:29 +0300

    repleaced localhost

--- components/auth_nav.tsx
+++ components/auth_nav.tsx
@@ -18,7 +18,7 @@ export default function AuthNav() {
 
   useEffect(() => {
     const api = async () => {
-      let Res = await fetch("http://localhost:3000/api/me", {
+      let Res = await fetch("https://soundly-peach.vercel.app/api/me", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
